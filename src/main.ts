4f11f96eea3762787e59f60f19dd21a5db769d82
lincolnwallace1 <lincolnwallace10@gmail.com>
2025-02-27 20:45:26 -0300

    main refactorin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+import { AppModule } from './app.module';
+class App {
+  private port = Number(process.env.API_PORT) || 8080;
+
+  constructor() {
+    this.init();
+  }
+
+  private async init() {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: true,
+    });
+
+    await app.listen(this.port).then(() => {
+      console.log(`Server running on ${this.port}`);
+    });
+  }
 }
-bootstrap();
+
+export default new App();
